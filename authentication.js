61b815a4f7b6cbe3dcd6a256a46d86221657a8b6
Bryan Arnold <barnold@wks-000395.corp.coveo.com>
2018-06-18 16:59:13 -0400

    Authentication working better, refresh/get token not working but authorization url works

--- authentication.js
+++ authentication.js
@@ -1,78 +1,121 @@
-const base64 = require('base-64');
-const index = require('./index');
+'use strict';
 
-const getAccessToken = (z, bundle) => {
-	const promise = z.request(`https://platformdev.cloud.coveo.com/oauth/token`, {
-		method: 'POST',
-		body: {
-			code: bundle.inputData.code,
-			grant_type: 'authorization_code',
-			redirect_uri: bundle.inputData.redirect_uri
-		},
-		headers: {
-			'content-type': 'application/x-www-form-urlencoded',
-			'Authorization': `Basic ${base64.encode(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET)}`
-		}
-	});
+const baseOauthUrl = 'https://platformdev.cloud.coveo.com/oauth';
+// To get your OAuth2 redirect URI, run `zapier describe` and update this variable.
+// Will looke like 'https://zapier.com/dashboard/auth/oauth/return/App123CLIAPI/'
+const redirectUri = 'https://zapier.com/dashboard/auth/oauth/return/App4771CLIAPI/';
 
-	// Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
-	return promise.then((response) => {
-		if (response.status !== 200) {
-			throw new Error('Unable to fetch access token: ' + z.JSON.stringify(response.content));
-		}
+const getAuthorizeURL = (z, bundle) => {
+  let url = `${baseOauthUrl}/authorize`;
 
-		const result = JSON.parse(response.content);
-		return {
-			access_token: result.access_token,
-			refresh_token: result.refresh_token
-		};
-	});
+  const urlParts = [
+    `response_type=token`,
+    `redirect_uri=${encodeURIComponent(bundle.inputData.redirect_uri)}`,
+    `realm=Platform`,
+    `client_id=${process.env.CLIENT_ID}`,
+    `scope=full`,
+    `state=oauth2`,
+  ];
+
+  const finalUrl = `${url}?${urlParts.join('&')}`;
+
+  z.console.log(finalUrl);  
+
+  return finalUrl;
+
 };
 
-const testAuth = (z, bundle) => {
-	// Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
-	// every user will have access to, such as an account or profile endpoint like /me.
-	const promise = z.request({
-		url: `https://platformdev.cloud.coveo.com/rest/templates/apikeys`,
-	});
+const getAccessToken = (z, bundle) => {
+  let url = `${baseOauthUrl}/token`;
 
-	// This method can return any truthy value to indicate the credentials are valid.
-	// Raise an error to show
-	return promise.then((response) => {
-		if (response.status !== 200) {
-			throw new Error('The access token you supplied is not valid');
-		}
-		return response;
-	});
+  const body = {
+    code: bundle.inputData.code,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'authorization_code',
+  };
+
+  const promise = z.request(url, {
+    method: 'POST',
+    body,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = z.JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+      refresh_token: result.refresh_token,
+      id_token: result.id_token,
+    };
+  });
+};
+
+const refreshAccessToken = (z, bundle) => {
+  let url = `${baseOauthUrl}/token`;
+
+  const body = {
+    refresh_token: bundle.authData.refresh_token,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'refresh_token',
+  };
+
+  const promise = z.request(url, {
+    method: 'POST',
+    body,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  return promise.then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Unable to fetch access token: ' + response.content);
+    }
+
+    const result = z.JSON.parse(response.content);
+    return {
+      access_token: result.access_token,
+      refresh_token: result.refresh_token,
+      id_token: result.id_token,
+    };
+  });
+};
+
+// The test call Zapier makes to ensure an access token is valid
+// UX TIP: Hit an endpoint that always returns data with valid credentials,
+// like a /profile or /me endpoint. That way the success/failure is related to
+// the token and not because the user didn't happen to have a recently created record.
+const testAuth = (z) => {
+  const promise = z.request({
+    url: 'https://platformdev.cloud.coveo.com/rest/templates/apikeys',
+  });
+
+  return promise.then((response) => {
+    if (response.status === 401) {
+      throw new Error('The access token you supplied is not valid');
+    }
+    return z.JSON.parse(response.content);
+  });
 };
 
 module.exports = {
-	type: 'oauth2',
-	oauth2Config: {
-		// Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
-		// Zapier generates the state and redirect_uri, you are responsible for providing the rest.
-		// Note: can also be a function that returns a string
-		authorizeUrl: {
-			url: 'https://platformdev.cloud.coveo.com/oauth/authorize',
-			params: {
-				client_id: '{{process.env.CLIENT_ID}}',
-				redirect_uri: '{{bundle.inputData.redirect_uri}}',
-				response_type: 'code',
-				realm: 'Platform',
-				scope: 'full'
-			}
-		},
-		// Step 2 of the OAuth flow; Exchange a code for an access token.
-		// This could also use the request shorthand.
-		getAccessToken: getAccessToken
-
-		// If there is a specific scope you want to limit your Zapier app to, you can define it here.
-		// Will get passed along to the authorizeUrl
-		// scope: 'read,write'
-	},
-	// The test method allows Zapier to verify that the access token is valid. We'll execute this
-	// method after the OAuth flow is complete to ensure everything is setup properly.
-	test: testAuth,
-
-	connectionLabel: '{{username}}'
+  type: 'oauth2',
+  connectionLabel: '{{username}}',
+  oauth2Config: {
+    authorizeUrl: getAuthorizeURL,
+    getAccessToken,
+    refreshAccessToken,
+    // Set so Zapier automatically checks for 401s and calls refreshAccessToken
+    autoRefresh: true,
+    // offline_access is necessary for the refresh_token
+  },
+  test: testAuth,
 };
