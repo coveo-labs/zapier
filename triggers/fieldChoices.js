07a3d62b3a2431f52b57d080c314764f195227cc
Jerome Devost <jdevost@coveo.com>
2018-07-27 16:55:25 -0400

    Add unit tests for Triggers

--- triggers/fieldChoices.js
+++ triggers/fieldChoices.js
@@ -8,9 +8,8 @@ const message = require('../messages');
 //without the trigger actual showing up in the app. This allows me to create dynamic dropdowns for the input users
 //have to input for fields to push content to. This reduces errors and is a much better user experience. This specific function
 //gets the fields used by a specific source, fills the input value with the field name, and displays the field name in a readable format.
-const getFieldChoicesForInput = (z, bundle) => {
-  
-  z.console.log('Bundle: ' , bundle.inputData);
+const perform = (z, bundle) => {
+  z.console.log('Bundle: ', bundle.inputData);
 
   //Request to Coveo to get fields that specified source uses in it's mappings. Source ID and Org ID must be
   //given by the user beforehand for this to work.
@@ -18,31 +17,38 @@ const getFieldChoicesForInput = (z, bundle) => {
     url: `https://${platform}/rest/organizations/${bundle.inputData.orgId}/sources/${bundle.inputData.sourceId}`,
     method: 'GET',
   });
-  
+
   //Handle response
-  return sourceFieldsPromise.then((response) => {
+  return sourceFieldsPromise
+    .then(response => {
+      if (response.status >= 400) {
+        throw new Error(
+          'Error getting field choices for dropdown. Please ensure the source and organization IDs have been selected already: ' +
+            z.JSON.parse(response.content).message +
+            ' Error Code: ' +
+            response.status
+        );
+      }
 
-    if(response.status >= 400){
-      throw new Error('Error getting field choices for dropdown. Please ensure the source and organization IDs have been selected already: ' + z.JSON.parse(response.content).message + ' Error Code: ' + response.status);
-    }
-  
-    const results = z.JSON.parse(response.content);
+      let results = z.JSON.parse(response.content);
+      results = results.mappings || [];
+      if (!results.map) {
+        // make sure it's an array
+        results = [];
+      }
 
-    //Only want the ids and names of the fields from this call. If the field has
-    //already been selected in another input box, ignore it, otherwise grab it.
-    let sourceFields = results.mappings.map(r => {
-      return {id: r.id, fieldName: r.fieldName};
-    });
+      //Only want the ids and names of the fields from this call. If the field has
+      //already been selected in another input box, ignore it, otherwise grab it.
+      results = results.map(r => ({ id: r.id, fieldName: r.fieldName }));
 
-    //Check to make sure that fields exist in the source as mappings,
-    //if not throw an error to the user.
-    if(!Array.isArray(sourceFields) || sourceFields.length == 0){
-      throw new Error(message.NO_FIELDS);
-    }
+      //Check to make sure that fields exist in the source as mappings,
+      //if not throw an error to the user.
+      if (!results.length) {
+        throw new Error(message.NO_FIELDS);
+      }
 
-    return sourceFields;
-  
-  })
+      return results;
+    })
     .catch(handleError);
 };
 
@@ -56,8 +62,8 @@ module.exports = {
     hidden: true, //Makes the trigger hidden. Don't remove.
   },
 
-  operation:{
-    perform: getFieldChoicesForInput,
-    canPaginate: true, //Incase the nuber of fields is very high, allos for the results to be displayed in pages
+  operation: {
+    perform,
+    canPaginate: true, //In case the number of results is very high, allows for the results to be displayed in pages
   },
 };
--- triggers/orgSources.js
+++ triggers/orgSources.js
@@ -8,42 +8,42 @@ const message = require('../messages');
 //without the trigger actual showing up in the app. This allows me to create dynamic dropdowns for the input users
 //have to input for fields to push content to. This reduces errors and is a much better user experience. This specific function
 //gets the possible push sources that are in the specified org, puts the source id in the input value, and displays the source name as in readable format.
-const getSourceChoicesForInput = (z, bundle) => {
-  
+const perform = (z, bundle) => {
   //Request to Coveo to get fields that specified source uses in it's mappings. Org ID must be
   //given by the user beforehand for this to work.
   const orgSourcesPromise = z.request({
     url: `https://${platform}/rest/organizations/${bundle.inputData.orgId}/sources`,
     method: 'GET',
   });
-  
+
   //Handle response
-  return orgSourcesPromise.then((response) => {
-
-    let orgSources = [];
-
-    if(response.status >= 400){
-      throw new Error('Error getting source choices for dropdown. Please ensure the organization ID has been selected already: ' + z.JSON.parse(response.content).message + ' Error Code: ' + response.status);
-    }
-  
-    const results = z.JSON.parse(response.content);
-     
-    //Only want source ids and names from this call, ignore sources that aren't push as well, since we can only use this app to push to a push source.
-    results.forEach(source => {
-      if(source.sourceType == 'PUSH'){
-        orgSources.push({'id': source.id, 'name': source.name});
+  return orgSourcesPromise
+    .then(response => {
+      if (response.status >= 400) {
+        throw new Error(
+          'Error getting source choices for dropdown. Please ensure the organization ID has been selected already: ' + z.JSON.parse(response.content).message + ' Error Code: ' + response.status
+        );
       }
-    });
 
-    //Check if the selected organization has any push sources 
-    //in it, if not throw an error.
-    if(!Array.isArray(orgSources) || orgSources.length == 0){
-      throw new Error(message.NO_PUSH_SOURCES);
-    }
+      let results = z.JSON.parse(response.content);
+      if (!results.map) {
+        // make sure it's an array
+        results = [];
+      }
 
-    return orgSources;
-  
-  })
+      // We can only use this app with Push sources.
+      results = results.filter(source => source.sourceType === 'PUSH');
+      // Only want source ids and names from this call
+      results = results.map(source => ({ id: source.id, name: source.name }));
+
+      //Check if the selected organization has any push sources
+      //in it, if not throw an error.
+      if (!results.length) {
+        throw new Error(message.NO_PUSH_SOURCES);
+      }
+
+      return results;
+    })
     .catch(handleError);
 };
 
@@ -57,8 +57,8 @@ module.exports = {
     hidden: true, //Makes the trigger hidden. Don't remove.
   },
 
-  operation:{
-    perform: getSourceChoicesForInput,
-    canPaginate: true, //Incase the nuber of fields is very high, allos for the results to be displayed in pages
+  operation: {
+    perform,
+    canPaginate: true, //In case the number of results is very high, allows for the results to be displayed in pages
   },
 };
